378ab5908fdb0564b4253071e48e4c18409f63a5
spritefullake <42ahmad@gmail.com>
2017-09-07 20:32:02 -0400

    more server fiddling

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
+      console.log("client: ",client," RESULT ", result);
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('/db', {results: result.rows} ); }
+       { response.render('pages/db', {results: result.rows} ); }
     });
   });
 });
